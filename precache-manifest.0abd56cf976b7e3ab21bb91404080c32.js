0487a1f603f11ff9f4368d479fa4794007792aa0
Amritansh Pandey <amritansh.pandey6@gmail.com>
2019-10-10 05:45:40 +0530

    Updates

---
+++ precache-manifest.0abd56cf976b7e3ab21bb91404080c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a15b2dcd3278a41c7fc250f782b28a6",
+    "url": "//amritanshpandey.github.io/whitepanda-production/index.html"
+  },
+  {
+    "revision": "b0f0d0ff11d2f9edf675",
+    "url": "//amritanshpandey.github.io/whitepanda-production/static/css/main.b54172a1.chunk.css"
+  },
+  {
+    "revision": "8dbbba2ce4bb6b16b84f",
+    "url": "//amritanshpandey.github.io/whitepanda-production/static/js/2.7e1157f6.chunk.js"
+  },
+  {
+    "revision": "b0f0d0ff11d2f9edf675",
+    "url": "//amritanshpandey.github.io/whitepanda-production/static/js/main.b897bd5b.chunk.js"
+  },
+  {
+    "revision": "7daaeaa65f83e9de8d2b",
+    "url": "//amritanshpandey.github.io/whitepanda-production/static/js/runtime-main.9f146b43.js"
+  }
+]);
